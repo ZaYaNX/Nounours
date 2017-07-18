4a5aac0800c7ed272206a6368a8341189474737c
Guillaume Secret <g.secret91@gmail.com>
2017-07-18 16:32:02 +0200

    Second commit, Prototype inc

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CameraController : MonoBehaviour
+{
+    Vector3 offset;
+    GameObject Player;
+    [SerializeField]
+    float minPosX;
+    [SerializeField]
+    float maxPosX;
+    [SerializeField]
+    float minPosY;
+    [SerializeField]
+    float maxPosY;
+    
+    void Start ()
+    {
+        Player = GameObject.Find("Player");
+        offset = transform.position - Player.transform.position;
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void LateUpdate()
+    {
+        Vector3 CameraPosition = Player.transform.position + offset;
+        if (CameraPosition.y < minPosY)
+        {
+            CameraPosition.y = minPosY;
+        }
+        if (CameraPosition.y > maxPosY)
+        {
+            CameraPosition.y = maxPosY;
+        }
+        if (CameraPosition.x < minPosX)
+        {
+            CameraPosition.x = minPosX;
+        }
+        if (CameraPosition.x > maxPosX)
+        {
+            CameraPosition.x = maxPosX;
+        }
+        transform.position = CameraPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+
+    public GameObject currentInterObj = null;
+
+    void Update() // Filipe doit me taper
+    {
+        if (Input.GetKeyDown(KeyCode.E) && currentInterObj != null)
+        {
+            Destroy(currentInterObj);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("InteracObject"))
+        {
+            Debug.Log(other.name);
+            currentInterObj = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("InteracObject"))
+        {
+            if (other.gameObject == currentInterObj)
+            {
+                currentInterObj = null;
+            }
+        }
+    }
+}
