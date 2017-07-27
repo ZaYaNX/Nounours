fe54410070fa2dd2b60b8b6ba9075a6bb58bac0e
Skiini <skiini03@gmail.com>
2017-07-27 18:44:11 +0200

    Now u can't move in diagonal

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,8 +8,8 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    bool mvHoz;
-    bool mvVer;
+    bool mvHoz = false;
+    bool mvVer = false;
 
 
 
@@ -22,17 +22,37 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-      if (Input.GetAxisRaw("Horizontal") > 0f || Input.GetAxisRaw("Horizontal") < -0f)
-          {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-          }
-          if (Input.GetAxisRaw("Vertical") > 0f || Input.GetAxisRaw("Vertical") < -0f)
-          {
-              transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-          }
+        if (Input.GetAxisRaw("Horizontal") > 0f || Input.GetAxisRaw("Horizontal") < -0f)
+        {
+
+            if (!mvVer)
+            {
+                transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+                mvHoz = true;
+            }
+
+        }
+        else
+        {
+            mvHoz = false;
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0f || Input.GetAxisRaw("Vertical") < -0f)
+        {
+            if (!mvHoz)
+            {
+                transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+                mvVer = true;
+            }
+            
+        }
+        else
+        {
+            mvVer = false;
+        }
 
 
 
     }
-    
+
 }
