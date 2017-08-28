ad33929c82d5fde23e76d5ad46b607488839a6c5
Guillaume Secret <g.secret91@gmail.com>
2017-08-28 18:32:24 +0200

    Fix Pause

---
+++ Assets/Scripts/DistanceEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class DistanceEnemy : MonoBehaviour
+{
+    PlayerController PlayerScript;
+
+    private void Start()
+    {
+        PlayerScript = PlayerScript.gameObject.GetComponent<>();
+        
+    }
+    
+
+    
+}
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Pause : MonoBehaviour
+
+{
+    [SerializeField]
+    GameObject PauseUI;
+    private bool Paused = false;
+    void Start()
+    {
+        PauseUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Joystick1Button7))
+        {
+            Paused = !Paused;
+        }
+        if (Paused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+        if (!Paused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void Reprendre()
+    {
+        Paused = false;
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("menu");
+        Paused = false;
+    }
+
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+}
